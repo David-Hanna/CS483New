6eeabac025469a49e00b697e67c00172a79fbb5e
Brad Cooper <bradcooper314@gmail.com>
2016-10-18 03:50:11 -0300

    Main game loop runs with its own state machine.

---
+++ Kartaclysm/StateMachine/Gameplay/StateRacing.h
@@ -0,0 +1,40 @@
+//------------------------------------------------------------------------
+// StateRacing
+// Author:	Bradley Cooper
+//	
+// Gameplay state for racing.
+//------------------------------------------------------------------------
+
+#ifndef STATE_RACING_H
+#define STATE_RACING_H
+
+#include "GameplayState.h"
+
+namespace Kartaclysm
+{
+	class StateRacing : public Kartaclysm::GameplayState
+	{
+	public:
+		//------------------------------------------------------------------------------
+		// Public methods.
+		//------------------------------------------------------------------------------
+		StateRacing();
+		virtual ~StateRacing();
+
+		// Inherited
+		void Enter(const std::map<std::string, std::string>& p_mContextParameters);
+		void Suspend(const int p_iNewState);
+		void Unsuspend(const int p_iPrevState);
+		void Update(const float p_fDelta);
+		void PreRender();
+		void Exit();
+
+	protected:
+		// Inherited
+		HeatStroke::GameObjectManager* m_pGameObjectManager;
+		bool m_bSuspended;
+	};
+} // namespace Kartaclysm
+
+#endif
+
