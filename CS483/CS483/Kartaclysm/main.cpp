1c60d6f93961ca52a2c299e3a948cfb9193a497b
David <dhanna@upei.ca>
2016-10-24 14:09:32 -0300

    Update main for testing OBJ and MTL

--- CS483/CS483/Kartaclysm/main.cpp
+++ CS483/CS483/Kartaclysm/main.cpp
@@ -9,18 +9,26 @@
 #include <cstdlib>
 
 #include "OBJFile.h"
+#include "MTLFile.h"
 
 int main(int argc, char* argv[])
 {
 	//assignment1::ExampleGame exampleGame;
 	//return exampleGame.Run();
 
-	HeatStroke::OBJFile file(std::string("HeatStroke/Services/OBJ/test.obj"));
-	file.ParseFile();
-
-#ifdef _DEBUG
-	std::cout << file.ToString();
-#endif
+//	HeatStroke::OBJFile objFile(std::string("HeatStroke/Services/OBJ/test.obj"));
+//	objFile.ParseFile();
+//
+//#ifdef _DEBUG
+//	std::cout << objFile.ToString() << "\n";
+//#endif
+//
+//	HeatStroke::MTLFile mtlFile(std::string("HeatStroke/Services/MTL/test.mtl"));
+//	mtlFile.ParseFile();
+//
+//#ifdef _DEBUG
+//	std::cout << mtlFile.ToString() << "\n";
+//#endif
 
 	system("pause");
 	return 0;
