7328d5a25e5904c1f19a94488c4e2b81bf80000d
David MacIntosh (Laptop) <drmacintosh@upei.ca>
2016-11-08 09:55:39 -0400

    Made collision detection actually work, for real this time

--- HeatStroke/Services/Collisions/CollisionManager.cpp
+++ HeatStroke/Services/Collisions/CollisionManager.cpp
@@ -7,6 +7,8 @@
 
 #include "CollisionManager.h"
 
+#include "EventManager.h"
+
 using namespace HeatStroke;
 
 CollisionManager* CollisionManager::s_pCollisionManagerInstance = nullptr;
@@ -105,7 +107,16 @@ void CollisionManager::CheckCollision(ComponentSphereCollider* p_pCollider1, Com
 
 	if (glm::length(pos1 - pos2) <= p_pCollider1->GetRadius() + p_pCollider2->GetRadius())
 	{
-		printf("Sphere-Sphere Collision!");
+		glm::vec3 contactPoint = pos1 - (pos2 * (p_pCollider1->GetRadius()/(p_pCollider1->GetRadius() + p_pCollider2->GetRadius())));
+
+		//printf("Sphere-Sphere Collision!");
+		Event* collisionEvent = new Event("Collision");
+		collisionEvent->SetGameObjectParameter("Object1GUID", p_pCollider1->GetGameObject()->GetGUID());
+		collisionEvent->SetGameObjectParameter("Object2GUID", p_pCollider2->GetGameObject()->GetGUID());
+		collisionEvent->SetFloatParameter("ContactPointX", contactPoint.x);
+		collisionEvent->SetFloatParameter("ContactPointY", contactPoint.y);
+		collisionEvent->SetFloatParameter("ContactPointZ", contactPoint.z);
+		EventManager::Instance()->QueueEvent(collisionEvent);
 	}
 }
 
@@ -137,57 +148,63 @@ void CollisionManager::CheckCollision(ComponentSphereCollider* p_pCollider1, Com
 		return;
 	}
 
-	glm::vec3 c = pos1;
-	glm::vec3 l1 = pos2 + (glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), p_pCollider2->GetSurfaceNormal()) * p_pCollider2->GetWidth());
-	glm::vec3 l2 = pos2 + (glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), p_pCollider2->GetSurfaceNormal()) * -p_pCollider2->GetWidth());
+	glm::vec3 normal = p_pCollider2->GetSurfaceNormal();
+	glm::vec3 contactPoint;
 
-	// Rearrange these points so that l1 is at origin, and the line is along the z-axis
-	c = c - l1;
-	l2 = l2 - l1;
-	l1 = glm::vec3(0.0f, 0.0f, 0.0f);
-
-	float rotAngle = glm::dot(glm::normalize(l2), glm::vec3(0.0f, 0.0f, -1.0f));
-	glm::vec3 rotAxis = glm::cross(glm::normalize(l2), glm::vec3(0.0f, 0.0f, -1.0f));
-	glm::quat rotation;
-	if (rotAngle < -0.999f)
+	// Find the contact point
+	if ((normal.x == 1.0f || normal.x == -1.0f) && normal.z == 0.0f)
 	{
-		rotAxis = glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), glm::normalize(l2));
-		rotation = glm::angleAxis(180.0f, rotAxis);
+		glm::vec3 l1 = pos2 + glm::vec3(0.0f, 0.0f, p_pCollider2->GetWidth());
+		glm::vec3 l2 = pos2 - glm::vec3(0.0f, 0.0f, p_pCollider2->GetWidth());
+
+		if (pos1.z >= l1.z)
+		{
+			contactPoint = glm::vec3(l1.x, pos1.y, l1.z);
+		}
+		else if (pos1.z <= l2.z)
+		{
+			contactPoint = glm::vec3(l2.x, pos1.y, l2.z);;
+		}
+		else
+		{
+			contactPoint = glm::vec3(pos2.x, pos1.y, pos1.z);
+		}
+	}
+	else if ((normal.z == 1.0f || normal.z == -1.0f) && normal.x == 0.0f)
+	{
+		glm::vec3 l1 = pos2 + glm::vec3(p_pCollider2->GetWidth(), 0.0f, 0.0f);
+		glm::vec3 l2 = pos2 - glm::vec3(p_pCollider2->GetWidth(), 0.0f, 0.0f);
+
+		if (pos1.x >= l1.x)
+		{
+			contactPoint = glm::vec3(l1.x, pos1.y, l1.z);
+		}
+		else if (pos1.x <= l2.x)
+		{
+			contactPoint = glm::vec3(l2.x, pos1.y, l2.z);;
+		}
+		else
+		{
+			contactPoint = glm::vec3(pos1.x, pos1.y, pos2.z);
+		}
 	}
 	else
 	{
-		float s = sqrtf((1 + rotAngle) * 2);
-		float invs = 1.0f / s;
-		rotation = glm::quat(s * 0.5f, rotAxis.x * invs, rotAxis.y * invs, rotAxis.z * invs);
+		assert(false && "Walls must be axis aligned!");
+		return;
 	}
 
-	c = c * rotation;
-	l2 = l2 * rotation;
-
-	if (l2.z < 0)
+	// See if we collide
+	if (glm::length(pos1 - contactPoint) <= radius)
 	{
-		c.z *= -1.0f;
-		l2.z *= -1.0f;
-	}
-
-	// Find the point on the line closest to the circle
-	glm::vec2 nearPoint;
-	if (c.z <= 0.0f)
-	{
-		nearPoint = glm::vec2(l1.x, l1.z);
-	}
-	else if (c.z >= l2.z)
-	{
-		nearPoint = glm::vec2(l2.x, l2.z);
-	}
-	else
-	{
-		nearPoint = glm::vec2(0.0f, c.z);
-	}
-
-	// See if the distance is less than the radius of the sphere
-	if (glm::length(nearPoint - glm::vec2(c.x, c.z)) <= radius)
-	{
-		printf("Sphere-Wall Collision! ");
+		//printf("Sphere-Wall Collision!\n");
+		//printf("%f, %f, %f\n", contactPoint.x, contactPoint.y, contactPoint.z);
+		Event* collisionEvent = new Event("Collision");
+		collisionEvent->SetGameObjectParameter("Object1GUID", p_pCollider1->GetGameObject()->GetGUID());
+		collisionEvent->SetGameObjectParameter("Object2GUID", p_pCollider2->GetGameObject()->GetGUID());
+		collisionEvent->SetFloatParameter("ContactPointX", contactPoint.x);
+		collisionEvent->SetFloatParameter("ContactPointY", contactPoint.y);
+		collisionEvent->SetFloatParameter("ContactPointZ", contactPoint.z);
+		EventManager::Instance()->QueueEvent(collisionEvent);
 	}
 }
