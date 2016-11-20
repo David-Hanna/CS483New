1461f7b20db81c4c909cd684c1052eaf848f92ef
David MacIntosh (Laptop) <drmacintosh@upei.ca>
2016-11-20 15:55:23 -0400

    WIP

---
+++ CS483/CS483/Kartaclysm/Components/ComponentTrackPiece.h
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------
+// ComponentTrackPiece
+//
+// Author:	David MacIntosh
+//	
+// Manages some useful info about a specific track piece
+//------------------------------------------------------------------------
+
+#ifndef COMPONENT_TRACK_PIECE_H
+#define COMPONENT_TRACK_PIECE_H
+
+#include "Component.h"
+#include "GameObject.h"
+
+namespace Kartaclysm
+{
+	class ComponentTrackPiece : public HeatStroke::Component
+	{
+		public:
+			~ComponentTrackPiece();
+
+			virtual const std::string FamilyID() const override { return "GOC_TrackPiece"; }
+			virtual const std::string ComponentID() const override { return "GOC_TrackPiece"; }
+
+			static HeatStroke::Component* CreateComponent(
+				HeatStroke::GameObject* p_pGameObject,
+				tinyxml2::XMLNode* p_pBaseNode,
+				tinyxml2::XMLNode* p_pOverrideNode);
+
+			virtual void Init() override;
+			virtual void Update(const float p_fDelta) override;
+
+		protected:
+			ComponentTrackPiece(HeatStroke::GameObject* p_pGameObject /* parameters */);
+
+		private:
+			float m_fWidth;
+			float m_fHeight;
+	};
+}
+
+#endif // COMPONENT_TRACK_PIECE_H
