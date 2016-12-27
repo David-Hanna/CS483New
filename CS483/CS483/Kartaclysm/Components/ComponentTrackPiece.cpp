da31295d6d97f26bd02eda0362c65b932304a0b5
Matt White <matt.t.white07@gmail.com>
2016-12-27 00:04:32 -0400

    Improved method for determining which racer is ahead

--- CS483/CS483/Kartaclysm/Components/ComponentTrackPiece.cpp
+++ CS483/CS483/Kartaclysm/Components/ComponentTrackPiece.cpp
@@ -8,14 +8,20 @@ namespace Kartaclysm
 		float p_fWidthZ,
 		HeightFunction p_eHeightFunction,
 		float p_fHeight1,
-		float p_fHeight2)
+		float p_fHeight2,
+		PositionFunction p_ePositionFunction,
+		glm::vec3 p_vPivotPosition,
+		glm::vec3 p_vPivotAxis)
 		:
 		Component(p_pGameObject),
 		m_fWidthX(p_fWidthX),
 		m_fWidthZ(p_fWidthZ),
 		m_eHeightFunction(p_eHeightFunction),
 		m_fHeight1(p_fHeight1),
-		m_fHeight2(p_fHeight2)
+		m_fHeight2(p_fHeight2),
+		m_ePositionFunction(p_ePositionFunction),
+		m_vPivotPosition(p_vPivotPosition),
+		m_vPivotAxis(p_vPivotAxis)
 	{
 	}
 
@@ -70,7 +76,16 @@ namespace Kartaclysm
 			}
 		}
 
-		return new ComponentTrackPiece(p_pGameObject, fWidthX, fWidthZ, eHeightFunction, fHeight1, fHeight2);
+		PositionFunction ePositionFunction = ParsePositionFunction(p_pBaseNode);
+		glm::vec3 vPivotPosition;
+		glm::vec3 vPivotAxis;
+		if (ePositionFunction == Turn)
+		{
+			vPivotPosition = ParsePivotPosition(p_pBaseNode);
+			vPivotAxis = ParsePivotAxis(p_pBaseNode);
+		}
+
+		return new ComponentTrackPiece(p_pGameObject, fWidthX, fWidthZ, eHeightFunction, fHeight1, fHeight2, ePositionFunction, vPivotPosition, vPivotAxis);
 	}
 
 	void ComponentTrackPiece::Init()
@@ -137,5 +152,109 @@ namespace Kartaclysm
 				return baseHeight;
 				break;
 		}
+	}
+
+	bool ComponentTrackPiece::IsAhead(const glm::vec3& p_vFirstRacerPosition, const glm::vec3& p_vSecondRacerPosition) const
+	{
+		switch (m_ePositionFunction)
+		{
+			case Straight:
+				return IsAheadOnStraight(p_vFirstRacerPosition, p_vSecondRacerPosition);
+			case Turn:
+				return IsAheadOnTurn(p_vFirstRacerPosition, p_vSecondRacerPosition);
+			default:
+				printf("Unknown position function\n");
+				break;
+		}
+	}
+
+	bool ComponentTrackPiece::IsAheadOnStraight(const glm::vec3& p_vFirstRacerPosition, const glm::vec3& p_vSecondRacerPosition) const
+	{
+		glm::vec4 vPlane = CalculatePlane();
+
+		float fFirstRacerDistance = GetDistanceToPlane(vPlane, p_vFirstRacerPosition);
+		float fSecondRacerDistance = GetDistanceToPlane(vPlane, p_vSecondRacerPosition);
+
+		return fFirstRacerDistance <= fSecondRacerDistance;
+	}
+
+	bool ComponentTrackPiece::IsAheadOnTurn(const glm::vec3& p_vFirstRacerPosition, const glm::vec3& p_vSecondRacerPosition) const
+	{
+		glm::vec3 vPivot = CalculatePivot();
+		glm::vec3 vPivotAxis = CalculatePivotAxis();
+
+		glm::vec3 v1 = glm::normalize(p_vFirstRacerPosition - vPivot);
+		glm::vec3 v2 = glm::normalize(p_vSecondRacerPosition - vPivot);
+
+		return glm::dot(vPivotAxis, v1) <= glm::dot(vPivotAxis, v2);
+	}
+
+	//TODO would be great to calculate this once and store it for all future uses
+	glm::vec4 ComponentTrackPiece::CalculatePlane() const
+	{
+		HeatStroke::HierarchicalTransform trackTransform = m_pGameObject->GetTransform();
+		glm::quat qTrackRotation = trackTransform.GetRotation();
+		qTrackRotation.y *= -1.0f;
+		glm::vec3 vNormal = glm::vec3(1.0f, 0.0f, 0.0f) * qTrackRotation;
+		glm::vec3 vPlaneCenter = trackTransform.GetTranslation() + (glm::vec3(-5.0f, 0.0f, 0.0f) * qTrackRotation);
+		return glm::vec4(vNormal, -(vNormal.x * vPlaneCenter.x) - (vNormal.y * vPlaneCenter.y) - (vNormal.z * vPlaneCenter.z));
+	}
+
+	float ComponentTrackPiece::GetDistanceToPlane(const glm::vec4& p_vPlane, const glm::vec3& p_vPoint) const
+	{
+		return ((p_vPlane.x * p_vPoint.x) + (p_vPlane.y * p_vPoint.y) + (p_vPlane.z * p_vPoint.z) + p_vPlane.w) /
+			(sqrt(pow(p_vPlane.x, 2) + pow(p_vPlane.y, 2) + pow(p_vPlane.z, 2)));
+	}
+
+	//TODO would be great to calculate this once and store it for all future uses
+	glm::vec3 ComponentTrackPiece::CalculatePivot() const
+	{
+		HeatStroke::HierarchicalTransform trackTransform = m_pGameObject->GetTransform();
+		glm::quat qTrackRotation = trackTransform.GetRotation();
+		qTrackRotation.y *= -1.0f;
+		return trackTransform.GetTranslation() + (m_vPivotPosition * qTrackRotation);
+	}
+
+	//TODO would be great to calculate this once and store it for all future uses
+	glm::vec3 ComponentTrackPiece::CalculatePivotAxis() const
+	{
+		HeatStroke::HierarchicalTransform trackTransform = m_pGameObject->GetTransform();
+		glm::quat qTrackRotation = trackTransform.GetRotation();
+		qTrackRotation.y *= -1.0f;
+		return glm::normalize(m_vPivotAxis * qTrackRotation);
+	}
+
+	PositionFunction ComponentTrackPiece::ParsePositionFunction(const tinyxml2::XMLNode* p_pNode)
+	{
+		std::string strHeightFunction = "";
+		HeatStroke::EasyXML::GetRequiredStringAttribute(p_pNode->FirstChildElement("Position"), "function", strHeightFunction);
+		if (strHeightFunction.compare("straight") == 0)
+		{
+			return Straight;
+		}
+		else if (strHeightFunction.compare("turn") == 0)
+		{
+			return Turn;
+		}
+	}
+
+	glm::vec3 ComponentTrackPiece::ParsePivotPosition(const tinyxml2::XMLNode* p_pNode)
+	{
+		glm::vec3 vPivotPosition;
+		const tinyxml2::XMLElement* pPositionElement = p_pNode->FirstChildElement("Position");
+		HeatStroke::EasyXML::GetRequiredFloatAttribute(pPositionElement, "pivotX", vPivotPosition.x);
+		HeatStroke::EasyXML::GetRequiredFloatAttribute(pPositionElement, "pivotY", vPivotPosition.y);
+		HeatStroke::EasyXML::GetRequiredFloatAttribute(pPositionElement, "pivotZ", vPivotPosition.z);
+		return vPivotPosition;
+	}
+
+	glm::vec3 ComponentTrackPiece::ParsePivotAxis(const tinyxml2::XMLNode* p_pNode)
+	{
+		glm::vec3 vPivotAxis;
+		const tinyxml2::XMLElement* pPositionElement = p_pNode->FirstChildElement("Position");
+		HeatStroke::EasyXML::GetRequiredFloatAttribute(pPositionElement, "pivotAxisX", vPivotAxis.x);
+		HeatStroke::EasyXML::GetRequiredFloatAttribute(pPositionElement, "pivotAxisY", vPivotAxis.y);
+		HeatStroke::EasyXML::GetRequiredFloatAttribute(pPositionElement, "pivotAxisZ", vPivotAxis.z);
+		return vPivotAxis;
 	}
 }
