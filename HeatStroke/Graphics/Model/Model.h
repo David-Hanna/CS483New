bee0ddc1af63d7f232aa86eade3ee9c5d344f02b
Matt White <matt.t.white07@gmail.com>
2017-04-05 03:32:37 -0300

    Updated loading of models to load data from custom binary format generated by command line tool

--- HeatStroke/Graphics/Model/Model.h
+++ HeatStroke/Graphics/Model/Model.h
@@ -31,6 +31,9 @@ namespace HeatStroke
 
 	private:
 		std::vector<Mesh> m_vMeshes;
+
+		// the signature of this method is gross, but it's better than having all of this code inlined
+		void AddToVertexData(std::vector<float>& p_vVertexData, const OBJFile::OBJVertex& p_Vertex, const std::vector<const glm::vec3>& p_vPositions, const std::vector<const glm::vec3>& p_vNormals, const std::vector<const glm::vec2>& p_vUVs);
 	};
 }
 
